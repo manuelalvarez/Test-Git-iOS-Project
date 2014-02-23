055992712b216a33df13ba2f39e7f9f9ebf97983
Manuel Alvarez <malvarez@Manuels-MacBook-Pro-2.local>
2014-02-23 16:55:26 -0400

    added a test class

---
+++ Git Test/MACTest.h
@@ -0,0 +1,13 @@
+//
+//  MACTest.h
+//  Git Test
+//
+//  Created by Manuel Alvarez on 2/23/14.
+//  Copyright (c) 2014 Manuel Alvarez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MACTest : NSObject
+
+@end
---
+++ Git Test/MACTest.m
@@ -0,0 +1,13 @@
+//
+//  MACTest.m
+//  Git Test
+//
+//  Created by Manuel Alvarez on 2/23/14.
+//  Copyright (c) 2014 Manuel Alvarez. All rights reserved.
+//
+
+#import "MACTest.h"
+
+@implementation MACTest
+
+@end
